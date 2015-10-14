c1c57596557f4a9fac61c095f161d1242ae25ce7
Blaalid <Blaalid@me.com>
2015-10-14 12:59:50 +0200

    added next button

--- view/bookingstepone.php
+++ view/bookingstepone.php
@@ -33,7 +33,7 @@
                                       
                                  
                             </div>
-                            <button class="btn btn-default" style="width:100%;background-color: #00ff00;" type="submit">Add</button>
+                            <button type="button" type="submit">Next</button>
                         </form>
                        
                     </div>
