3fcc3e44f1dd9093a2709fd7e8ee0557dce8f569
Blaalid <Blaalid@me.com>
2015-10-14 13:22:17 +0200

    Made the input field as required to advance to the next step

--- view/bookingstepone.php
+++ view/bookingstepone.php
@@ -17,19 +17,19 @@
                     <div id="main-top-overlay-booking">
                         <form action="?page=bookingsteptwo" method="post">
                             <div id="form-left-column">
-                                <label>Gender</label> <input type="text" name="givenGender" placeholder="Mr/Mrs" tabindex="1"><br>
-                                <label>First name</label> <input type="text" name="givenFirst_name" placeholder="Enter first name" tabindex="2"><br>                     
-                                <label>Address</label> <input type="text" name="givenStreet_address" placeholder="Enter address line" tabindex="4"><br>                           
-                                <label>City</label> <input type="text" name="givenCity" placeholder="Enter City" tabindex="6"><br>                               
-                                <label>E-mail</label> <input type="text" name="givenEmail" placeholder="Enter e-mail address" tabindex="10"><br> 
+                                <label>Gender</label> <input type="text" name="givenGender" placeholder="Mr/Mrs" tabindex="1" required><br>
+                                <label>First name</label> <input type="text" name="givenFirst_name" placeholder="Enter first name" tabindex="2" required><br>                     
+                                <label>Address</label> <input type="text" name="givenStreet_address" placeholder="Enter address line" tabindex="4" required><br>                           
+                                <label>City</label> <input type="text" name="givenCity" placeholder="Enter City" tabindex="6" required><br>                               
+                                <label>E-mail</label> <input type="text" name="givenEmail" placeholder="Enter e-mail address" tabindex="10" required><br> 
                             </div>
                         
                             
                             <div id="form-right-column">
-                                 <label>Last name</label> <input type="text" name="givenLast_name" placeholder="Enter last name" tabindex="3"><br>
-                                 <label>ZIP code</label> <input type="text" name="givenZip_code" placeholder="Enter ZIP code" tabindex="5"><br>
-                                 <label>Country</label> <input type="text" name="givenCountry" placeholder="Enter country" tabindex="7"><br>
-                                 <label>Telephone</label> <input type="text" name="givenPhone_number" placeholder="Enter telephone number" tabindex="9"><br>
+                                 <label>Last name</label> <input type="text" name="givenLast_name" placeholder="Enter last name" tabindex="3" required><br>
+                                 <label>ZIP code</label> <input type="text" name="givenZip_code" placeholder="Enter ZIP code" tabindex="5" required><br>
+                                 <label>Country</label> <input type="text" name="givenCountry" placeholder="Enter country" tabindex="7" required><br>
+                                 <label>Telephone</label> <input type="text" name="givenPhone_number" placeholder="Enter telephone number" tabindex="9" required><br>
                                       
                                  
                             </div>
