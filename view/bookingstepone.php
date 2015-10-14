b277d0af27f02841f31116b4cbacb40b42b2d1e4
Blaalid <Blaalid@me.com>
2015-10-14 12:38:36 +0200

    Added new input design

--- view/bookingstepone.php
+++ view/bookingstepone.php
@@ -1,26 +1,37 @@
+<?php
+//////////////////////////////////////////
+// Template for customer listing page
+//////////////////////////////////////////
+
+// TODO - initialize necessary variables here (remember to pass them in the controller's render() function)
+// Expected variables:
+// $customers - list of all customers
+// $customerName - last value used in "Add customer" form
+//$Customers = $GLOBALS["customers"];
+//$CustomerName = $GLOBALS["customerName"];
+?>
 <div id="main-top-booking">
                     
+                    <div id="main-top-booking">
+                    
                     <div id="main-top-overlay-booking">
-                        <form action="?page=bookingTwo" method="post">
+                        <form action="?page=bookingsteptwo" method="post">
                             <div id="form-left-column">
-                                Gender <input type="text" name="givenGender" placeholder="Mr/Mrs" tabindex="1"><br>
-                                First name <input type="text" name="givenFirst_name" placeholder="Enter first name" tabindex="2"><br>
-                                Last name <input type="text" name="givenLast_name" placeholder="Enter last name" tabindex="3"><br>
-                                Address<input type="text" name="givenStreet_address" placeholder="Enter address line" tabindex="4"><br>
-                                ZIP code <input type="text" name="givenZip_code" placeholder="Enter ZIP code" tabindex="5"><br>
-                                City <input type="text" name="givenCity" placeholder="Enter City" tabindex="6"><br>
-                                Country <input type="text" name="givenCountry" placeholder="Enter country" tabindex="7"><br>
-                                AreaCode<input type="text" name="givenArea_code" placeholder="Enter areaCode" tabindex="8"><br>
-                                Telephone<input type="text" name="givenPhone_number" placeholder="Enter telephone number" tabindex="9"><br>
-                                E-mail <input type="text" name="givenEmail" placeholder="Enter e-mail address" tabindex="10"><br>
+                                <label>Gender</label> <input type="text" name="givenGender" placeholder="Mr/Mrs" tabindex="1"><br>
+                                <label>First name</label> <input type="text" name="givenFirst_name" placeholder="Enter first name" tabindex="2"><br>                     
+                                <label>Address</label> <input type="text" name="givenStreet_address" placeholder="Enter address line" tabindex="4"><br>                           
+                                <label>City</label> <input type="text" name="givenCity" placeholder="Enter City" tabindex="6"><br>                               
+                                <label>E-mail</label> <input type="text" name="givenEmail" placeholder="Enter e-mail address" tabindex="10"><br> 
                             </div>
+                        
                             
                             <div id="form-right-column">
-                                
-                                                        
-                                
-                               
-                                                                                              
+                                 <label>Last name</label> <input type="text" name="givenLast_name" placeholder="Enter last name" tabindex="3"><br>
+                                 <label>ZIP code</label> <input type="text" name="givenZip_code" placeholder="Enter ZIP code" tabindex="5"><br>
+                                 <label>Country</label> <input type="text" name="givenCountry" placeholder="Enter country" tabindex="7"><br>
+                                 <label>Telephone</label> <input type="text" name="givenPhone_number" placeholder="Enter telephone number" tabindex="9"><br>
+                                      
+                                 
                             </div>
                             <button class="btn btn-default" style="width:100%;background-color: #00ff00;" type="submit">Add</button>
                         </form>
@@ -28,4 +39,5 @@
                     </div>
                 
                     
-               </div>   
+                </div>   
+               
