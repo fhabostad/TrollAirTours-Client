dab0b71c1e9ac6465416485c3540326b1e5b43af
fhabostad <fredrik.habostad@stud.hials.no>
2015-10-21 22:54:03 +0200

    ChangeReviewFormat

--- view/bookingstepFour.php
+++ view/bookingstepFour.php
@@ -12,19 +12,22 @@
                         
         
                             <div id="form-left-column">
-                            Please review your booking information<br>
-                            Gender :<?php echo "".$_SESSION['givenGender']?><br>
-                            Birth date :<?php echo "".$_SESSION['givenBirth_date']?><br>
-                            First Name :<?php echo "".$_SESSION['givenFirst_name']?><br>
-                            Last Name :<?php echo "".$_SESSION['givenLast_name']?><br>
-                            Address :<?php echo "".$_SESSION['givenStreet_address']?><br>    
-                            Zip Code :<?php echo "".$_SESSION['givenZip_code']?><br>
-                            City :<?php echo "".$_SESSION['givenCity']?><br>
-                            Country :<?php echo "".$_SESSION['givenCountry']?><br>
+                                <h1>Please review your booking information</h1><br>
+                            
+                                <h2>Personal details:</h2>    
+                            Gender : <?php echo "".$_SESSION['givenGender']?> Born: <?php echo "".$_SESSION['givenBirth_date']?><br>
+                            Name : <?php echo $_SESSION['givenLast_name'] . ", " . $_SESSION['givenFirst_name']?><br>
+                            Address : <?php echo "".$_SESSION['givenStreet_address'] . ", ".$_SESSION['givenZip_code'] . ", " . $_SESSION['givenCity']?><br>    
+                            Country : <?php echo "".$_SESSION['givenCountry']?><br>
                             <br>
-                            Country Code: <?php echo "".$_SESSION['givenCountry_code']?><br>
-                            Phone: <?php echo "".$_SESSION['givenPhone_number']?><br>
+                            Phone: <?php echo "+".$_SESSION['givenCountry_code']. " ". $_SESSION['givenPhone_number']?><br>
                             Email: <?php echo "".$_SESSION['givenEmail']?> 
+                            <br>                           
+                            <h2>Extra Products</h2>
+                            Drinks :<?php echo "".$_SESSION['ProductName']?><br>
+                            Food :<?php echo "".$_SESSION['givenFoodName'];?><br>
+                            Dutyfree :<?php echo "".$_SESSION['ProductName']?><br>          
                             </div>
+ <?php
 
-                                      
+ ?>
