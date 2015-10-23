33f2bc1e5b0dfeb2d5cae87af068390196b168a4
fhabostad <fredrik.habostad@stud.hials.no>
2015-10-23 15:43:06 +0200

    asdefrth

--- view/bookingstepthree.php
+++ view/bookingstepthree.php
@@ -21,19 +21,19 @@
                                                         <option value="Male" >Male</option>
                                                         <option value="Female">Female</option>
                                 </select>
-                                <label>First name</label> <input type="text" name="givenFirst_name" placeholder="Enter first name" tabindex="2" required><br>
-                                <label>Birth date</label> <input type="date" name="givenBirth_date" placeholder="Enter birth date YYYY-MM-DD" max="2000-12-31" min="1899-01-01" tabindex="4" required><br>  
-                                <label>City</label> <input type="text" name="givenCity" placeholder="Enter City" tabindex="6" required><br>                   
-                                <label>ZIP code</label> <input type="number" name="givenZip_code" placeholder="Enter ZIP code" min="0" tabindex="8" required><br>        
-                                <label>Country code</label> <input type="text" name="givenCountry_code" placeholder="Enter country code" tabindex="10" required><br> 
+                                <label>First name</label> <input type="text" name="givenFirst_name" value="<?php print "".$_SESSION['givenFirst_name']?>" placeholder="Enter first name" tabindex="2" ><br>
+                                <label>Birth date</label> <input type="date" name="givenBirth_date" value="<?php print "".$_SESSION['givenBirth_name']?>" placeholder="Enter birth date YYYY-MM-DD" max="2000-12-31" min="1899-01-01" tabindex="4" required><br>  
+                                <label>City</label> <input type="text" name="givenCity" value="<?php print "".$_SESSION['givenCity']?>" placeholder="Enter City" tabindex="6" required><br>                   
+                                <label>ZIP code</label> <input type="number" name="givenZip_code" value="<?php print "".$_SESSION['givenZip_code']?>" placeholder="Enter ZIP code" min="0" tabindex="8" required><br>        
+                                <label>Country code</label> <input type="text" name="givenCountry_code" value="<?php print "".$_SESSION['givenCountry_code']?>" placeholder="Enter country code" tabindex="10" required><br> 
                             </div>
 
                             <div id="form-right-column">
-                                 <label>Last name</label> <input type="text" name="givenLast_name" placeholder="Enter last name" tabindex="3" required><br>
-                                 <label>Country</label> <input type="text" name="givenCountry" placeholder="Enter country" tabindex="5" required><br>
-                                 <label>Address</label> <input type="text" name="givenStreet_address" placeholder="Enter address line" tabindex="7" required><br>                                                  
-                                 <label>E-mail</label> <input type="email" name="givenEmail" placeholder="Enter e-mail address" tabindex="9" required><br> 
-                                 <label>Telephone</label> <input type="number" name="givenPhone_number" placeholder="Enter telephone number" min="0" tabindex="11" required><br>
+                                 <label>Last name</label> <input type="text" name="givenLast_name" value="<?php print "".$_SESSION['givenLast_name']?>" placeholder="Enter last name" tabindex="3" required><br>
+                                 <label>Country</label> <input type="text" name="givenCountry" value="<?php print "".$_SESSION['givenCountry']?>" placeholder="Enter country" tabindex="5" required><br>
+                                 <label>Address</label> <input type="text" name="givenStreet_address" value="<?php print "".$_SESSION['givenStreet_address']?>" placeholder="Enter address line" tabindex="7" required><br>                                                  
+                                 <label>E-mail</label> <input type="email" name="givenEmail" value="<?php print "".$_SESSION['givenEmail']?>" placeholder="Enter e-mail address" tabindex="9" required><br> 
+                                 <label>Telephone</label> <input type="number" name="givenPhone_number" value="<?php print "".$_SESSION['givenPhone_number']?>" placeholder="Enter telephone number" min="0" tabindex="11" required><br>
                                       
                                  
                             </div>
