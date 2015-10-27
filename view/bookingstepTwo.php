26f3305131e9f432f39d5a6d456bbb822e398fa0
Blaalid <Blaalid@me.com>
2015-10-27 16:09:57 +0100

    Added radiobuttons for seat reservation

--- view/bookingstepTwo.php
+++ view/bookingstepTwo.php
@@ -7,16 +7,66 @@
  */
 
 ?>
-
+        
 
 
 <div id="main-top-booking">
     <div id="main-top-overlay-booking">
-        <h1>Seteres!</h1>
-    </div>
+
+    <ul class="select-seat">
+        <li>
+            <input type="radio" id="A1" value="A1" name="seatnumber" />
+            <label for="A1">A1</label>
+        </li>
+        <li>
+            <input type="radio" id="A2" value="A2" name="seatnumber"/>
+            <label for="A2">A2</label>
+        </li>
+        <li>
+            <input type="radio" id="A3" value="A3" name="seatnumber"/>
+            <label for="A3">A3</label>
+        </li>
+        <li>
+            <input type="radio" id="A4" value="A4" name="seatnumber"/>
+            <label for="A4">A4</label>
+        </li>
+        <li>
+            <input type="radio" id="A5" value="A5" name="seatnumber"/>
+            <label for="A5">A5</label>
+        </li>
+        <li>
+            <input type="radio" id="A6" value="A6" name="seatnumber"/>
+            <label for="A6">A6</label>
+        </li>
+        <li>
+            <input type="radio" id="B1" value="B1" name="seatnumber"/>
+            <label for="B1">B1</label>
+        </li>
+        <li>
+            <input type="radio" id="B2" value="B2" name="seatnumber"/>
+            <label for="B2">B2</label>
+        </li>
+        <li>
+            <input type="radio" id="B3" value="B3" name="seatnumber"/>
+            <label for="B3">B3</label>
+        </li>
+        <li>
+            <input type="radio" id="B4" value="B4" name="seatnumber"/>
+            <label for="B4">B4</label>
+        </li>
+        <li>
+            <input type="radio" id="B5" value="B5" name="seatnumber"/>
+            <label for="B5">B5</label>
+        </li>
+        <li>
+            <input type="radio" id="B6" value="B6" name="seatnumber"/>
+            <label for="B6">B6</label>
+        </li>
+
+    </ul>  
+ </div>
     
 </div>
-
 
 <div id="main-bottom-booking">
                     <ul>
