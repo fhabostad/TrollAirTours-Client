af2b31c5c498d4c37a2411971a22205083578d8f
Jørgen Nikolai Havnsund <Jorgen.havnsund@live.no>
2015-10-27 16:13:42 +0100

    chchchanges

--- view/bookingCustomSummary.php
+++ view/bookingCustomSummary.php
@@ -3,23 +3,31 @@
 <div id="main-top-booking">
     <div id="main-top-overlay-booking">
         <div id="bookingCustom">
-        <h2>Summary</h2>
-         <div id="bookingCustomSummary">
+            <div id="main-top-overlay-summary">    
+        <div id="bookingCustomSummary">
+                                <h2>Summary</h2>
+                                <h3>Flight details</h3>
+                                <label>Destination: <?php echo $_SESSION["givenCustomDestination"];?></label>  
+                                <label>Date: <?php echo $_SESSION["givenPreferredDate"];?> </label>  
+                                <label>Time: <?php echo $_SESSION["givenPreferredTime"];?> </label> 
+                                <label>Guide: <?php echo $_SESSION["givenGuide"];?> </label>
+                                
+                               
                                 <h3>Peronal details</h3>
-                                <label>Company: </label> <?php echo $_SESSION["givenCompany"];?><br> 
-                                <label>First name: </label> <?php echo $_SESSION["givenFirst_name"];?><br> 
-                                <label>Last name: </label> <?php echo $_SESSION["givenLast_name"];?><br> 
+                                <label>Company: <?php echo $_SESSION["givenCompany"];?></label>  
+                                <label>First name: <?php echo $_SESSION["givenFirst_name"];?> </label>  
+                                <label>Last name: <?php echo $_SESSION["givenLast_name"];?> </label> 
                                  
                                 <h3>Contact information</h3>
-                                <label>E-mail: </label> <?php echo $_SESSION["givenEmail"];?><br> 
-                                <label>Telephone: </label> <?php echo $_SESSION["givenPhone_number"];?><br> 
+                                <label>E-mail: <?php echo $_SESSION["givenEmail"];?></label>  
+                                <label>Telephone: <?php echo $_SESSION["givenPhone_number"];?></label>  
+            
                                 <p>Please check that all the details are correct. :)</p>
-                                                                          
-                                 
                                  
                                       
                                  
          </div>
+            </div>
         </div>       
     </div> 
 </div>
