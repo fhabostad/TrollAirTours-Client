e30328f49d265509174eb10536fd367af0747ae6
Blaalid <Blaalid@me.com>
2015-10-29 15:49:17 +0100

    text change

--- view/home.php
+++ view/home.php
@@ -7,22 +7,23 @@
                             <p> Geiranger is a small tourist village in Sunnmøre region of Møre og Romsdal county in the western part of Norway. </p> 
                             <p> It lies in Stranda Municipality at the head of the Geirangerfjorden, which is a branch of the large Storfjorden. </p> 
                             <p> The nearest city is Ålesund. Geiranger is home to some of the most spectacular scenery in the world, and has been named the best travel destination in Scandinavia by Lonely Planet. The Seven Sisters waterfall is located just west of Geiranger.</p>
-                            <a href="https://no.wikipedia.org/wiki/Geiranger"> More information </a>    
+                            <a href="https://en.wikipedia.org/wiki/Geiranger"> More information </a>    
                         </div>
                         
                         
                         <div id="briksdalen-info">
                             <h2> Briksdalen </h2>
-                            <p>Briksdalsbreen (English: the Briksdal glacier) is one of the most accessible and best known arms of the Jostedalsbreen glacier. Briksdalsbreen is located in the municipality of Stryn in Sogn og Fjordane county, Norway. 
-                                It is located inside Jostedalsbreen National Park. Briksdalsbreen terminates in a small glacial lake, Briksdalsbrevatnet, which lies 346 metres (1,135 ft) above sea level.[1]
-                            <a href="https://no.wikipedia.org/wiki/Briksdalen"> More information </a>    
+                            <p> In Briksdalen lies Briksdalsbreen (the Briksdal glacier) and is one of the most accessible and best known arms of the Jostedalsbreen glacier. Briksdalsbreen is located in the municipality of Stryn in Sogn og Fjordane county, Norway. 
+                            <p> It is located inside Jostedalsbreen National Park. Briksdalsbreen terminates in a small glacial lake, Briksdalsbrevatnet, which lies 346 metres (1,135 ft) above sea level.</p>
+                            <p> The size of Briksdalsbreen depends not only on temperature, but is also strongly affected by precipitation. </p>
+                            <a href="https://en.wikipedia.org/wiki/Briksdalsbreen"> More information </a>    
                         </div>
                         
                         <div id ="Aakneset-info">
                             <h2> Åkneset </h2>
-                            <p> Åkneset is a small tourist village in Sunnmøre region of Møre og Romsdal county in the western part of Norway. </p> 
-                            <p> It lies in Stranda Municipality at the head of the Geirangerfjorden, which is a branch of the large Storfjorden. </p> 
-                            <p> The nearest city is Ålesund. Geiranger is home to some of the most spectacular scenery in the world, and has been named the best travel destination in Scandinavia by Lonely Planet. The Seven Sisters waterfall is located just west of Geiranger.</p>
+                            <p> Åkneset is a mountain massif under Flosteinnibba in Stranda, Møre og Romsdal.</p> 
+                            <p> This area is very unstable, and a landslide from this massif can create a tidal wave as high as 80 meters. Small sensors are placed in the area to measure the movement of the mountain. </p> 
+                            <p> This is place is also the starting point of the tidal wave in the norwegian movie "Bølgen"</p>
                             <a href="https://no.wikipedia.org/wiki/Åkneset"> More information </a>    
                         </div>
                         
@@ -30,7 +31,6 @@
                         <div id="egendefinert-info">
                             <h2> Custom destination </h2>
                             <p>Egendefinert er en nydelig plass i Stryn kommune, jallatext atext atext </p>
-                            <a href="https://no.wikipedia.org/wiki/Briksda"> More information </a>    
                         </div>
                      
                         
