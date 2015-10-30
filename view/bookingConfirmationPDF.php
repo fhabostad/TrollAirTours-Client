d66eed1e72210d4523a90a79f0c3634eb555961f
fhabostad <fredrik.habostad@stud.hials.no>
2015-10-30 09:11:46 +0100

    SessionKILL

--- view/bookingConfirmationPDF.php
+++ view/bookingConfirmationPDF.php
@@ -126,10 +126,40 @@ $mail->addAttachment($filename) ;
 
 if (!$mail->send()) {
    echo "Mailer Error: " . $mail->ErrorInfo;
+   // Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
+session_destroy();
    
 } else {
    unlink($filename);
    unlink($urlRelativeFilePath);
+   // Unset all of the session variables.
+    $_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+    if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
+    session_destroy();
 
    exit();
    //echo "Message sent!";
