f804eeceb968fd21779e7422f258e443e2ce564d
Blaalid <Blaalid@me.com>
2015-11-02 12:40:26 +0100

    hometext

--- view/home.php
+++ view/home.php
@@ -6,7 +6,7 @@
                             <h2> Geiranger </h2>
                             <p> Geiranger is a small tourist village in Sunnmøre region of Møre og Romsdal county in the western part of Norway. </p> 
                             <p> It lies in Stranda Municipality at the head of the Geirangerfjorden, which is a branch of the large Storfjorden. </p> 
-                            <p> The nearest city is Ålesund. Geiranger is home to some of the most spectacular scenery in the world, and has been named the best travel destination in Scandinavia by Lonely Planet. The Seven Sisters waterfall is located just west of Geiranger.</p>
+                            <p> The trip to Geiranger will bring you close to some of the most spectacular scenery in the world. The Seven Sisters waterfall is one of them.</p>
                             <a href="https://en.wikipedia.org/wiki/Geiranger"> More information </a>    
                         </div>
                         
@@ -14,8 +14,8 @@
                         <div id="briksdalen-info">
                             <h2> Briksdalen </h2>
                             <p> In Briksdalen lies Briksdalsbreen (the Briksdal glacier) and is one of the most accessible and best known arms of the Jostedalsbreen glacier. Briksdalsbreen is located in the municipality of Stryn in Sogn og Fjordane county, Norway. 
-                            <p> It is located inside Jostedalsbreen National Park. Briksdalsbreen terminates in a small glacial lake, Briksdalsbrevatnet, which lies 346 metres (1,135 ft) above sea level.</p>
                             <p> The size of Briksdalsbreen depends not only on temperature, but is also strongly affected by precipitation. </p>
+                            <p> Briksdalsbreen's amazing view will send shivers down your spine as you fly past the light blue glacier.</p>
                             <a href="https://en.wikipedia.org/wiki/Briksdalsbreen"> More information </a>    
                         </div>
                         
@@ -23,7 +23,7 @@
                             <h2> Åkneset </h2>
                             <p> Åkneset is a mountain massif under Flosteinnibba in Stranda, Møre og Romsdal.</p> 
                             <p> This area is very unstable, and a landslide from this massif can create a tidal wave as high as 80 meters. Small sensors are placed in the area to measure the movement of the mountain. </p> 
-                            <p> This is place is also the starting point of the tidal wave in the norwegian movie "Bølgen"</p>
+                            <p> The trip to Åkneset will also show you the starting point of the tidal wave in the norwegian movie "Bølgen" that will most likely happen in real life.</p>
                             <a href="https://no.wikipedia.org/wiki/Åkneset"> More information </a>    
                         </div>
                         
