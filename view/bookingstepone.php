7e69b8c22b27e770c32c0655ad21294e35f2e8eb
Blaalid <Blaalid@me.com>
2015-11-13 15:48:17 +0100

    Comments and some cleanup

--- view/bookingstepone.php
+++ view/bookingstepone.php
@@ -10,70 +10,63 @@ $dutyfrees = $GLOBALS["dutyfrees"];
 
 
 ?>
-
-
-
-
 <script>
 
-
+//A jQuery widget for selecting, displaying and holding dates
 $(function() {
  $( "#datepicker" ).datepicker({
  beforeShowDay: EnableSpecificDates,
  dateFormat: "yy-mm-dd"
-
- 
  });
  });
 
-
-
-function pad(d) {
+/*
+*if the variable 'd', the day of the month is before the 10th add a zero to prevent calendar crash
+*then return as a string
+*/
+    function pad(d) {
     return (d < 10) ? '0' + d.toString() : d.toString();
 }
  
-   
+/*
+*Checks which destination that is checked, runs through a foreach loop for that destination and prints 
+*its enabled dates
+*/
 function EnableSpecificDates(date) {
-
-
-if(document.getElementById('Geiranger').checked) {
-  var disableddates = [<?php foreach($calenderDatesGeiranger as $calenderDateGeiranger){ echo  "\"$calenderDateGeiranger\"" . ",";} ?>];
-}else if(document.getElementById('Briksdalen').checked) {
-  var disableddates = [<?php foreach($calenderDatesBriksdalen as $calenderDateBriksdalen){ echo  "\"$calenderDateBriksdalen\"" . ",";} ?>];
-  
-}else if(document.getElementById('Aakneset').checked) {
-  var disableddates = [<?php foreach($calenderDatesAakneset as $calenderDateAakneset){ echo  "\"$calenderDateAakneset\"" . ",";} ?>];
+    if(document.getElementById('Geiranger').checked) {
+      var disableddates = [<?php foreach($calenderDatesGeiranger as $calenderDateGeiranger){ echo  "\"$calenderDateGeiranger\"" . ",";} ?>];
+    }else if(document.getElementById('Briksdalen').checked) {
+      var disableddates = [<?php foreach($calenderDatesBriksdalen as $calenderDateBriksdalen){ echo  "\"$calenderDateBriksdalen\"" . ",";} ?>];
+    }else if(document.getElementById('Aakneset').checked) {
+      var disableddates = [<?php foreach($calenderDatesAakneset as $calenderDateAakneset){ echo  "\"$calenderDateAakneset\"" . ",";} ?>];
 }     
-      
-disableddates.push("9999-12-12"); //Adds a date to prevent calender crash.
 
+disableddates.push("9999-12-12"); //Adds a date to prevent calender crash.
 
  var m = date.getMonth();
  var d = pad(date.getDate());
  var y = date.getFullYear();
- 
   
  var currentdate = y + "-" + (m+1) + "-" + d;
  //window.alert(currentdate);
  for (var i = 0; i < disableddates.length; i++) {
  if ($.inArray(currentdate, disableddates) !== -1) {
-     //window.alert(currentdate);
+ //window.alert(currentdate);
  return [true];
- }else
+    }else
  {
      return [false];
  }
  }
-
 }
 
-
-
- 
- 
+ /*
+ * Sets a variable equal to 'TimeList' and runs a for loop that removes its elements.
+ * It clears the time drop down menu before selecting the destination.
+ */
  function clearTimeDrop()
  {
-     var x = document.getElementById("TimeList");
+    var x = document.getElementById("TimeList");
     var i;
     for(i=x.options.length-1;i>=0;i--)
     {
@@ -81,7 +74,16 @@ disableddates.push("9999-12-12"); //Adds a date to prevent calender crash.
     } 
  }
 
- function timedrop()
+/*
+* Calls the clearTimeDrop() function to clear time and checks which destination is checked,
+* then sets it as the selected tour type.
+* Gets the selected date and runs through a foreach loop that checks if the flight date
+* is set to the selected date and the tour type is set to the selected tour type. 
+* If that is true, the flight ID and departure time will be added to the TimeList.
+* The function getPrice() is called after the destination, date and time is selected to
+* ensure the right price is given.
+*/
+function timedrop()
  {
     clearTimeDrop();
     
@@ -93,10 +95,7 @@ disableddates.push("9999-12-12"); //Adds a date to prevent calender crash.
     }else if(document.getElementById('Aakneset').checked) {
         selectedTourType = "Aakneset";
     }   
-    
-    
-    
-    
+ 
     var x = document.getElementById("TimeList");
     var  selectedDate = document.getElementById('datepicker').value;
     
@@ -109,16 +108,19 @@ disableddates.push("9999-12-12"); //Adds a date to prevent calender crash.
             option.text = "<?php echo $flightTimeAndDate["Departure"] ?>";
             x.add(option);
         }
-        
     <?php } ?>
     
      getPrice();
  }
 
-
+/*
+ * Clears the TimeList.
+ * Checks the selected destination and displays the same form for Geiranger, Briksdalen and
+ * Aakneset. If custom is checked, a predefined custom-form is displayed.
+ */
 function switchForm()
 {
-    document.getElementById('datepicker').value = "";
+  //  document.getElementById('datepicker').value = "";
     document.getElementById('datepicker').value = "";
     clearTimeDrop();
     var e = document.getElementById('preDefTour');
@@ -149,6 +151,11 @@ function switchForm()
     } 
 }
 
+/*
+ * Runs through a foreach loop to check if flight ID is set to selectedFlight,
+ * if that is true, flightPrice is set to be FlightPrice and then set the Price
+ * as flightPrice.
+ */
 function getPrice()
 {
     var selectedFlight = document.getElementById('TimeList').value;
@@ -160,15 +167,19 @@ function getPrice()
             var flightPrice = "<?php echo $flightTimeAndDate["FlightPrice"] ?>";
             var p =document.getElementById('Price'); 
             p.value = flightPrice;
-           
-           
         }
-        
     <?php } ?>
-    getTotalPrice();
+    
+        getTotalPrice();
     }    
     
-    
+/*
+ * Checks with foreach loop on foods, drinks and dutyfrees if they contain
+ * a productID. If that is true the ProductPrice will be set to selected price for 
+ * that category.
+ * The total price is then set to zero and as long as a category's price is not undefined
+ * it will be added to the total price.
+ */
 function getTotalPrice()
  {
      var selectFood = document.getElementById('Food'); 
@@ -177,7 +188,6 @@ function getTotalPrice()
      var selectedDrinkID = selectDrink.value;
      var selectDutyFree = document.getElementById('Dutyfree'); 
      var selectedDutyFreeID = selectDutyFree.value;
-
      
      <?php foreach($foods as $food){?>
          
@@ -193,7 +203,6 @@ function getTotalPrice()
          {
              var selectedDrinkPrice =  <?php echo $drink["ProductPrice"] ?>;
          }   
-                
      <?php } ?>
      
      <?php foreach($dutyfrees as $dutyfree){?>
@@ -202,7 +211,6 @@ function getTotalPrice()
          {
              var selectedDutyFree =  <?php echo $dutyfree["ProductPrice"] ?>;
          }   
-                
      <?php } ?>
      
      var p =document.getElementById('Price'); 
@@ -210,8 +218,6 @@ function getTotalPrice()
 
         var totalPrice = 0;
    
-   
-     
      if(typeof flightPrice != 'undefined')
      {
          totalPrice += +flightPrice;
@@ -222,7 +228,6 @@ function getTotalPrice()
          totalPrice += +selectedDrinkPrice;
      }
      
-  
      if(typeof selectedFoodPrice != 'undefined')
      {
          totalPrice += +selectedFoodPrice;
@@ -236,23 +241,16 @@ function getTotalPrice()
      var y =document.getElementById('TotalPrice'); 
             
      y.value = totalPrice;
-        
-    
  }
-
-
 
 </script>
 
 <div id="main-top-booking">
 <div id="main-top-overlay-booking">
 
-
-    
 <div id='CircleLocation'>
     <div class='circleBaseActive'>
         <span> 1 </span> 
-            
     </div>
      <div class='circleBase'>
          <span> 2 </span> 
@@ -265,76 +263,51 @@ function getTotalPrice()
     </div>
 </div>
 <form action="?page=bookingTwo" id="bookingTwo" method="post">
-
-    
     <div id="destination-buttons"> 
         <input type="radio" name="givenDestination" id="Geiranger" value="Geiranger" checked="checked" onclick="switchForm()" > Geiranger </input>
         <input type="radio" name="givenDestination" id="Briksdalen" value="Briksdalen" onclick="switchForm()"> Briksdalen </input>
         <input type="radio" name="givenDestination" id="Aakneset" value="Aakneset" onclick="switchForm()"> Aakneset </input>
         <input type="radio" name="givenDestination" id="Custom" value="Custom" onclick="switchForm()"> Custom </input>
     </div>
-    
     <div id ="preDefTour">
+        <div id="date-and-time">
+            <label for="inputDate" >Select date</label>
+                <input readonly  type="text" id="datepicker" name="givenDate" placeholder="Select Date" onchange="timedrop()">
+            <label for="inputTime" >Select time</label>
+                <select id="TimeList" name="selectedFlightID" onchange="getPrice()" >
+                    <option disabled selected>- Select time -</option>
+                </select>
+            <label for="inputPrice"></label>
+                <input type="hidden" readonly  type="text" id="Price" name="givenPrice" value="0" ></input>
+        </div>
+            <div id="product-form">
+                    <label for="inputProductID" >Select your desired products</label>
 
-    <div id="date-and-time">
-        
-        <label for="inputDate" >Select date</label>
-        <input readonly  type="text" id="datepicker" name="givenDate" placeholder="Select Date" onchange="timedrop()">
-        
-        <label for="inputTime" >Select time</label>
-        <select id="TimeList" name="selectedFlightID" onchange="getPrice()" >
-            <option disabled selected>- Select time -</option>
-        </select>
-       
-        <label for="inputPrice"></label>
-       <input type="hidden" readonly  type="text" id="Price" name="givenPrice" value="0" ></input>
-    </div>
-       
-            
+                    <select id="Food" name="givenFoodID" onchange="getTotalPrice()">
+                       <option value="None" selected>- no food selected -</option>
+                        <?php foreach($foods as $food): ?> 
+                                 <option value="<?php echo $food["ProductID"];?>"><?php echo $food["ProductName"];?>  <?php echo " NOK " . $food["ProductPrice"].",-" ;  ?></option>
+                        <?php endforeach; ?>
+                    </select>
 
+                    <select id="Drink" name="givenDrinkID" onchange="getTotalPrice()">
+                      <option value="None" selected>- no drink selected -</option>
+                        <?php foreach($drinks as $drink): ?> 
+                       <option value="<?php echo $drink["ProductID"]; ?>"><?php echo $drink["ProductName"];?>  <?php echo " NOK " . $drink["ProductPrice"].",-" ; ?></option>
+                        <?php endforeach; ?>
+                    </select>
 
-                        <div id="product-form">
-
-                            <label for="inputProductID" >Select your desired products</label>
-                         
-                            <select id="Food" name="givenFoodID" onchange="getTotalPrice()">
-                               <option value="None" selected>- no food selected -</option>
-                                <?php foreach($foods as $food): ?> 
-                                         <option value="<?php echo $food["ProductID"];?>"><?php echo $food["ProductName"];?>  <?php echo " NOK " . $food["ProductPrice"].",-" ;  ?></option>
-                                <?php endforeach; ?>
-                            </select>
-
-                            <select id="Drink" name="givenDrinkID" onchange="getTotalPrice()">
-                              <option value="None" selected>- no drink selected -</option>
-                                <?php foreach($drinks as $drink): ?> 
-                               <option value="<?php echo $drink["ProductID"]; ?>"><?php echo $drink["ProductName"];?>  <?php echo " NOK " . $drink["ProductPrice"].",-" ; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-
-                            <select id="Dutyfree" name="givenDutyFreeID" onchange="getTotalPrice()">
-                              <option value="None" selected>- no duty free selected -</option>
-                                <?php foreach($dutyfrees as $dutyfree): ?> 
-                                         <option value="<?php echo $dutyfree["ProductID"]; ?>"><?php echo $dutyfree["ProductName"];?>  <?php echo " NOK " . $dutyfree["ProductPrice"].",-" ; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                        <label for="inputTotalPrice">Price NOK:</label>
-                        <input readonly  type="text" id="TotalPrice" name="givenTotalPrice" value="0" ></input>
-                            </div>
-        
-                        
-    </div>
-            
-        
-        
-        
+                    <select id="Dutyfree" name="givenDutyFreeID" onchange="getTotalPrice()">
+                      <option value="None" selected>- no duty free selected -</option>
+                        <?php foreach($dutyfrees as $dutyfree): ?> 
+                                 <option value="<?php echo $dutyfree["ProductID"]; ?>"><?php echo $dutyfree["ProductName"];?>  <?php echo " NOK " . $dutyfree["ProductPrice"].",-" ; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                <label for="inputTotalPrice">Price NOK:</label>
+                <input readonly  type="text" id="TotalPrice" name="givenTotalPrice" value="0" ></input>
+            </div>
+        </div>
     </form>
-
-
-
-
-    
-    
-  
 
 <div id="Custom-form-stepone">
     <div id="infobox">
@@ -344,35 +317,28 @@ function getTotalPrice()
                 If you're unsure about which destinations and dates that can be chosen, 
                 please use the links for more information. <br>
                 <a href='http://localhost/TrollAirTours-Client/?page=about' target='blank'> More Information </a>
-                
             </p>
         </div>
     <form action="?page=bookingCustom" id="bookingStepOneCustom" method="post">
-    <div id="Custom-form-left">
+        <div id="Custom-form-left">
         
-        <label>Destination</label> <br> <input id="input-grow" type="text" name="givenCustomDestination" placeholder="Enter preferred destinations" tabindex="1" required draggable="true"> <a href="#" onclick='toggle_visiblity_infobox()'>(Help)</a><br>
-        <label>Date</label> <br> <input id="date" type="text" name="givenPreferredDate" placeholder="Enter pref. (YYYY-MM-DD)" max="2030-31-12" min="2015-12-10" tabindex="2" required><br>
-        <label>Time</label> <br> <input id="time" type="text" name="givenPreferredTime" placeholder="Enter preferred (hh:mm)" max="23:59" min="00:00" tabindex="3" required><br>                                                  
-    <label>Guide</label> <br> <select id="DropdownGuide" type="text" name="givenGuide" placeholder="Select language" tabindex="4" required>
-     <option id="0" index="0" value="select" > -- Select language -- </option>
-            <option id="1" index="1" value="No guide" >No guide</option>
-            <option id="2" index="2" value="English" >English</option>
-            <option id="3" index="3" value="Norwegian">Norwegian</option>
-            <option id="4" index="4" value="Chinese">Chinese</option>
-            <option id="5" index="5" value="Spanish">Spanish</option>
-            <option id="6" index="6" value="French">French</option>
-    </select>
-    </div>
+            <label>Destination</label> <br> <input id="input-grow" type="text" name="givenCustomDestination" placeholder="Enter preferred destinations" tabindex="1" required draggable="true"> <a href="#" onclick='toggle_visiblity_infobox()'>(Help)</a><br>
+            <label>Date</label> <br> <input id="date" type="text" name="givenPreferredDate" placeholder="Enter pref. (YYYY-MM-DD)" max="2030-31-12" min="2015-12-10" tabindex="2" required><br>
+            <label>Time</label> <br> <input id="time" type="text" name="givenPreferredTime" placeholder="Enter preferred (hh:mm)" max="23:59" min="00:00" tabindex="3" required><br>                                                  
+            <label>Guide</label> <br> <select id="DropdownGuide" type="text" name="givenGuide" placeholder="Select language" tabindex="4" required>
+               <option id="0" index="0" value="select" > -- Select language -- </option>
+               <option id="1" index="1" value="No guide" >No guide</option>
+               <option id="2" index="2" value="English" >English</option>
+               <option id="3" index="3" value="Norwegian">Norwegian</option>
+               <option id="4" index="4" value="Chinese">Chinese</option>
+               <option id="5" index="5" value="Spanish">Spanish</option>
+               <option id="6" index="6" value="French">French</option>
+            </select>
+        </div>
     </form>
-
 </div>
-
-    </div>
-
-
 </div>
-
- 
+</div>
                 <div id="main-bottom-booking">
                     <ul>
                         <li id="previous-booking-step">
@@ -381,11 +347,9 @@ function getTotalPrice()
                             </a>
                         </li>
                         <li id="next-booking-step">
-                            <!--<div id="preDefTourNext"><a href="javascript:{}" onclick="document.getElementById('bookingTwo').submit();"><h3>Next</h3></a></div> -->
                             <div id="preDefTourNext"><a href="#" onclick="validateFormStepOne()"><h3>Next</h3></a></div>
                             <div id="preDefTourNext"><a href="#" onclick="validateStepOneCustom()"><h3>Next</h3></a></div>
                         </li>
-                        
                     </ul>
                 </div>
                      
